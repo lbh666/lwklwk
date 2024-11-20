86254eb4e4fcab1349f052aacbd640100b8cf537
Alan Liao <60417779+lbh666@users.noreply.github.com>
2024-11-20 17:51:02 +0800

    Update BLDA.py

--- BLDA.py
+++ BLDA.py
@@ -1,17 +1,10 @@
-# Obtained from: https://github.com/open-mmlab/mmsegmentation/tree/v0.16.0
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from ..builder import LOSSES
 from .utils import get_class_weight, weight_reduce_loss
 import numpy as np
-from sklearn.mixture import GaussianMixture
-from collections import namedtuple
 import numpy as np
-from scipy.stats import norm
-import time
 import random
 
 
@@ -32,11 +25,6 @@ def cross_entropy(pred,
         weight=class_weight,
         reduction='none',
         ignore_index=ignore_index)
-    
-    # ## focal
-    # gama = 2
-
-    # loss = (1-torch.exp(-loss))**gama  *loss
 
     # apply weights and do the reduction
     if weight is not None:
@@ -150,83 +138,7 @@ def mask_cross_entropy(pred,
         pred_slice, target, weight=class_weight, reduction='mean')[None]
 
 
-@LOSSES.register_module()
-class CrossEntropyLoss(nn.Module):
-    """CrossEntropyLoss.
 
-    Args:
-        use_sigmoid (bool, optional): Whether the prediction uses sigmoid
-            of softmax. Defaults to False.
-        use_mask (bool, optional): Whether to use mask cross entropy loss.
-            Defaults to False.
-        reduction (str, optional): . Defaults to 'mean'.
-            Options are "none", "mean" and "sum".
-        class_weight (list[float] | str, optional): Weight of each class. If in
-            str format, read them from a file. Defaults to None.
-        loss_weight (float, optional): Weight of the loss. Defaults to 1.0.
-    """
-
-    def __init__(self,
-                 use_sigmoid=False,
-                 use_mask=False,
-                 reduction='mean',
-                 class_weight=None,
-                 loss_weight=1.0):
-        super(CrossEntropyLoss, self).__init__()
-        assert (use_sigmoid is False) or (use_mask is False)
-        self.use_sigmoid = use_sigmoid
-        self.use_mask = use_mask
-        self.reduction = reduction
-        self.loss_weight = loss_weight
-        self.class_weight = get_class_weight(class_weight)
-
-        if self.use_sigmoid:
-            self.cls_criterion = binary_cross_entropy
-        elif self.use_mask:
-            self.cls_criterion = mask_cross_entropy
-        else:
-            self.cls_criterion = cross_entropy
-
-    def forward(self,
-                cls_score,
-                label,
-                weight=None,
-                avg_factor=None,
-                reduction_override=None,
-                **kwargs):
-        """Forward function."""
-        assert reduction_override in (None, 'none', 'mean', 'sum')
-        
-        
-        if isinstance(cls_score,list):
-            reg_score = cls_score[1]
-            cls_score = cls_score[0]
-            
-            
-        reduction = (
-            reduction_override if reduction_override else self.reduction)
-        if self.class_weight is not None:
-            class_weight = cls_score.new_tensor(self.class_weight)
-        else:
-            class_weight = None
-        loss_cls = self.loss_weight * self.cls_criterion(
-            cls_score,
-            label,
-            weight,
-            class_weight=class_weight,
-            reduction=reduction,
-            avg_factor=avg_factor,
-            **kwargs)
-        return loss_cls
-
-
-
-
-
-
-        
-# from scipy.interpolate import interp1d
-   
 
 
 # Define a function to approximate the calculation of the CDF of a normal distribution
@@ -397,7 +309,7 @@ def calculate_min_category_pixel_count(labels, min_required_count, ignored_categ
     # return min_required_count
     return min_pixel_count.item()  
 
-@LOSSES.register_module()
+
 class CrossEntropyLoss_BLDA(nn.Module):
     """CrossEntropyLoss.
 
